8872babeb10306b71a37cdac439c87476c329605
deep3001 <sonisudama901@gmail.com>
2025-10-29 07:43:56 +0530

    second commit

---
+++ 7.CHAPTER LOOPS/main.py
@@ -0,0 +1,16 @@
+# for i in range(1,10):
+#     print(i)
+
+
+# fruits = ["apple","banana","orange","greps","mango"]
+
+# # for i,t in enumerate(fruits):
+# #     print(i,t)
+
+
+fruits = ["apple","banana","orange","greps","mango"]
+
+cars = ['tata','suzuki','maruti','kia']
+
+for x,y in zip(fruits,cars):
+    print(x,y)
